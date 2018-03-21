a78ef09851e3829ce0aa150f5c68379d12cc530d
JD Leshan <stritch@gmail.com>
2018-03-21 18:55:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,20 @@
 class Application
   @@items = []
 
-  def call(env)'
+  def call(env)
     resp = Rack::Response.new(
     req = Rack::Request.new(env)
 
-    path = req.path.match(/items/)
-    item = req.path.split('/').last
+    path = req.path
+    item_name = req.path.split('/').last
     if path.include? "item"
-      if @@items.include? path
-        @@items.each  {|item| resp.write "item.price}\n"}
+      found_item = @@items.select {|item| item.name == item_name}.first
+      if found_item 
+        resp.write(found_item.price)
         resp.status = 200
       else
-      resp.write "Item not found"
-      resp.status = 400
+        resp.write "Item not found"
+        resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 400
